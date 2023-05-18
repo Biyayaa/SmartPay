8651c8365323672d57f7f371fbf483fe2ea5da07
Biyayaa <onaleye.ap@gmail.com>
2023-05-18 13:02:37 +0100

    transaction pin confirmation

--- JS/signup.js
+++ JS/signup.js
@@ -30,55 +30,48 @@ function signUp(ev) {
     alert("Passwords do not match");
     return;
   }
-   else {
-    function accountNumber() {
-        let randomNumber = Math.floor(Math.random() * 9999999999);
-        let accountNumber = randomNumber.toString();
-        return accountNumber;
-      }
-    // Retrieve registered users from local storage
-    let registeredUsers =
-      JSON.parse(localStorage.getItem("registeredUsers")) || [];
 
-    // Check if user already exists
-    let userExists = registeredUsers.some(function (user) {
-      return user.email === email || user.phoneNumber === phoneNumber;
-    });
-
-    if (userExists) {
-      alert("User with the same email or phone number already exists");
-      return;
-    }
-
-    // Save new user to registered users
-    let newUser = {
-      firstName: firstName,
-      lastName: lastName,
-      email: email,
-      phoneNumber: phoneNumber,
-      password: password,
-      accountNumber: accountNumber(), // Assign the generated accountNo to accountNumber
-      balance: 10000.0,
-      pin: null,
-    };
-    registeredUsers.push(newUser);
-
-    // Update registered users in local storage
-    localStorage.setItem("registeredUsers", JSON.stringify(registeredUsers));
-
-    alert("Signup successful");
-    signupForm.reset();
-
-    setTimeout(() => {
-      window.location.href = ".././html/accloading.html";
-    }, 2000);
+  function accountNumber() {
+    let randomNumber = Math.floor(Math.random() * 9999999999);
+    let accountNumber = randomNumber.toString();
+    return accountNumber;
   }
+
+  let registeredUsers =
+    JSON.parse(localStorage.getItem("registeredUsers")) || [];
+
+  let userExists = registeredUsers.some(function (user) {
+    return user.email === email || user.phoneNumber === phoneNumber;
+  });
+
+  if (userExists) {
+    alert("User with the same email or phone number already exists");
+    return;
+  }
+
+  let newUser = {
+    firstName: firstName,
+    lastName: lastName,
+    email: email,
+    phoneNumber: phoneNumber,
+    password: password,
+    accountNumber: accountNumber(),
+    balance: 10000.0,
+    pin: null,
+  };
+  registeredUsers.push(newUser);
+
+  localStorage.setItem("registeredUsers", JSON.stringify(registeredUsers));
+
+  alert("Signup successful");
+  signupForm.reset();
+
+  setTimeout(() => {
+    window.location.href = ".././html/accloading.html";
+  }, 2000);
 }
 
-
-// Retrieve registered users from local storage
 let registeredUsers = JSON.parse(localStorage.getItem("registeredUsers")) || [];
-
 let numberBox = document.getElementById("numberBox");
 
 registeredUsers.forEach(function (user) {
@@ -87,5 +80,46 @@ registeredUsers.forEach(function (user) {
 });
 
 function next() {
-    window.location.href = ".././html/setpin.html"
+  window.location.href = ".././html/setpin.html";
+}
+
+function updatePin() {
+  let pinBoxValue = document.getElementById("pinBox").value;
+  let confirmPinBoxValue = document.getElementById("confirmpinBox").value;
+  let registeredUsers =
+    JSON.parse(localStorage.getItem("registeredUsers")) || [];
+
+  let currentUserIndex = registeredUsers.findIndex(function (user) {
+    return user.pin === null;
+  });
+
+  if (currentUserIndex === -1) {
+    alert("Pin has already been set for the current user");
+    return;
+  }
+
+  // Ensure pinBoxValue and confirmPinBoxValue contain only numbers
+  if (!/^\d+$/.test(pinBoxValue) || !/^\d+$/.test(confirmPinBoxValue)) {
+    alert("Please enter only numeric values for the pin");
+    return;
+  }
+
+  // Limit pinBoxValue and confirmPinBoxValue to a maximum of four digits
+  if (pinBoxValue.length > 4 || confirmPinBoxValue.length > 4) {
+    alert("The pin should not exceed four digits");
+    return;
+  }
+
+  if (pinBoxValue === confirmPinBoxValue) {
+    registeredUsers[currentUserIndex].pin = pinBoxValue;
+    localStorage.setItem("registeredUsers", JSON.stringify(registeredUsers));
+
+    alert("Transaction pin set successfully");
+    window.location.href = ".././html/login.html"
+  } else {
+    alert("Transaction pins do not match");
+  }
+
+  
+  
 }
